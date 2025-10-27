d63b7a4bab97ac4f98c92f08f38e4dd6ca5ab055
Santiago Luque Villanueva <luquevillanuevasantiago@gmail.com>
2025-10-27 17:15:23 +0100

    Done

--- src/App.java
+++ src/App.java
@@ -1,5 +1,17 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Porfavor introduzca 10 numeros enteros.");
+
+        int neg = 0;
+        int pos = 0;
+
+        for(int i = 0; i < 10; i++){
+           if (Integer.parseInt(System.console().readLine()) < 0 ){
+            neg++;
+           } else {
+            pos++;
+           }
+        }
+        System.out.println("Hay "+pos+" números positivos y "+neg+" números negativos.");
     }
 }
